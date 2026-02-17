f8070d2e5a3d2c9115a0b61fa9f345ff19556801
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-02-17 17:27:41 +0530

    refactor name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
             <div className="flex items-center space-x-3 cursor-pointer">
               <Home className="w-8 h-8 text-orange-400 drop-shadow-lg" />
               <h1 className="text-4xl font-extrabold tracking-widest uppercase">
-                WDM<span className="text-orange-400">STORE</span>
+                E-COM<span className="text-orange-400">STORE</span>
               </h1>
             </div>
           </Link>
@@ -25,7 +25,11 @@ const Navbar = () => {
               className="relative p-3 bg-orange-500/10 rounded-xl hover:bg-orange-500/20 transition duration-200 border border-orange-400/50 shadow-lg cursor-pointer"
             >
               <ShoppingCart className="w-6 h-6 text-orange-400" />
-              {cartCount > 0 && <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full min-w-[24px] h-[20px]">{cartCount}</span>}
+              {cartCount > 0 && (
+                <span className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full min-w-[24px] h-[20px]">
+                  {cartCount}
+                </span>
+              )}
             </Link>
           </nav>
         </div>
